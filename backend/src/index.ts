51a1c7767062b383ec56e871bb98beafbb524483
EpicAryan <adaryan0811@gmail.com>
2024-03-20 12:06:50 +0530

    feat: Implement user registration functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
 
 mongoose.connect(process.env.MONGO_DB as string).then(() => {
     console.log("Connected to MongoDB");
@@ -14,10 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", async(req: Request, res: Response) => {
-    res.json({ message: "Hello World" });
-});
+app.use("/api/users", userRoutes);
+
 
 app.listen(3000, () => {
+    
     console.log("Server running on port 3000");
 });
